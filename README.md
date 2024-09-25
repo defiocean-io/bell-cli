68c77c28635254036fee67474fb1213ad28161fd
Normalizex <104392358+Normalizex@users.noreply.github.com>
2024-09-26 00:06:35 +0300

    init files
    + basic android alarms by termux api

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # bell-cli
+***
+# Get Started
+1. **Clone repo:**
+    ```
+    git clone https://github.com/defiocean-io/bell-cli.git
+    ```
+
+2. **Install dependencies:**
+    ```sh
+    npm install
+    ```
+3. **Run:**
+    ```sh
+    node index.js
+    ```
+4. **Authorize**
+    ```sh
+    [00:00:00] [>] Bell-Cli key login
+    prompt: cli_key:  *****
+    [00:00:00] Logged as: [User]
+    ```
+***
+# Usage
+1. **Run:**
+    ```sh
+    node index.js
+    ```
+2. **Authorize**
+    ```sh
+    [00:00:00] [>] Bell-Cli key login
+    prompt: cli_key:  *****
+    [00:00:00] Logged as: [User]
+    ```
---
+++ index.js
@@ -0,0 +1,86 @@
+import fs from 'fs';
+import chalk from 'chalk';
+import axios from 'axios';
+import prompt from 'prompt';
+import BigNumber from 'bignumber.js';
+import { exec } from 'child_process';
+
+const timelog = (message) => console.log(`[${new Date().toLocaleTimeString()}] ${message}`);
+
+const bootstrap = async() => {
+    let logged = false;
+
+    let storage = await new Promise((resolve) => {
+        fs.readFile('storage.json', {}, (err, data) => {
+            if (err) {
+                fs.writeFile('storage.json', JSON.stringify({ token: '' }), {}, (err) => {
+                    if (err) throw new Error('Error with create storage.json');
+    
+                    else {
+                        resolve({token: '' });
+                    }
+                });
+            }
+
+            if (data){
+                resolve(JSON.parse(data.toString()));
+            }
+        });
+    });
+
+    while (true){
+        if (!storage.token){
+            timelog(`${chalk.cyan('[>]')} ${chalk.blue('Bell-Cli')} key login`);
+            prompt.start();
+
+            const cliprompt = await prompt.get('cli_key');
+            await axios.get('http://bell.defiocean.io/api/user/me', { params: { token: cliprompt.cli_key } }).then((response) => {
+                storage.token = cliprompt.cli_key;
+
+                fs.writeFile('storage.json', JSON.stringify(storage), {}, (err) => {
+                    if (err) throw new Error('Update storage.json failed');
+                });
+            }).catch(() => {
+                timelog(`${chalk.red(`Authorization failed`)}`);
+            });
+
+            continue;
+        }
+
+        await axios.get('http://bell.defiocean.io/api/user/me', { params: { token: storage.token }}).then((response) => {
+            if (!logged){
+                logged = true;
+                timelog(`${chalk.green(`Logged as: ${response.data.name}`)}`);
+            }
+
+            const user = response.data;
+
+            user.alerts.forEach((alert) => {
+                if (alert.active && alert.targetMod === 'lesser' && new BigNumber(alert.currentValue).lt(alert.targetValue)){
+                    timelog(`${alert.targetSymbol} price: ${alert.currentValue}$ lesser than target ${alert.targetValue}$`);
+                    exec(`termux-toast "${alert.targetSymbol} price: ${alert.currentValue}$ lesser than target ${alert.targetValue}$" && termux-vibrate -d 3000`, (error, stdout, stderr) => {
+                        if (error) {
+                            timelog(`Command Failed: ${error.message}`);
+                            return;
+                        }
+                        timelog(`Command executed ${stdout}`);
+                    });
+                }
+                else if (alert.active && alert.targetMod === 'greater' && new BigNumber(alert.currentValue).gt(alert.targetValue)){
+                    timelog(`${alert.targetSymbol} price: ${alert.currentValue}$ greater than target ${alert.targetValue}$`);
+                    exec(`termux-toast "${alert.targetSymbol} price: ${alert.currentValue}$ greater than target ${alert.targetValue}$" && termux-vibrate -d 3000`, (error, stdout, stderr) => {
+                        if (error) {
+                            timelog(`Command Failed: ${error.message}`);
+                            return;
+                        }
+                        timelog(`Command executed ${stdout}`);
+                    });
+                }
+            });
+        })
+
+        await new Promise(r => setTimeout(r, 1000));
+    }
+};
+
+bootstrap();
